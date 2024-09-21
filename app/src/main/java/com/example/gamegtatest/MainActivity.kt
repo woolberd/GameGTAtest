7010f085d84e776291795f6633d128348cd0af3c
Ayana Tazhibaeva <parkna.ra5798@gmail.com>
2024-09-21 14:35:53 +0300

    new updates

--- app/src/main/java/com/example/gamegtatest/MainActivity.kt
+++ app/src/main/java/com/example/gamegtatest/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.gamegtatest
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.gamegtatest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mainBinding: ActivityMainBinding
+  private lateinit var mainBinding: ActivityMainBinding
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +23,9 @@ class MainActivity : AppCompatActivity() {
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
+
         if (savedInstanceState == null) {
-            val myFragment = DeathFragment()
+            val myFragment = MilitaryIDFragment()
 
             supportFragmentManager.beginTransaction()
                 .replace(R.id.main, myFragment)
